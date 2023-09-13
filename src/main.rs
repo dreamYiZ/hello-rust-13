171501f3ff619c3bed52b760a010bc9080e428bb
Rentoo <dreamYiZ@outlook.com>
2023-09-14 00:33:45 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -155,7 +155,6 @@ fn main() {
     let number = if condition { 5 } else { 6 };
 
     loop {
-        
         println!("again");
         break;
     }
@@ -165,12 +164,77 @@ fn main() {
     let result = loop {
         counter += 1;
         if counter == 10 {
-            break counter*2;
+            break counter * 2;
         }
     };
 
     println!("result is: {}", result);
 
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [1, 2, 3, 4, 5];
+
+    let mut a_index = 0;
+    while a_index < 5 {
+        println!("value at index {} is {}", a_index, a[a_index]);
+        a_index += 1;
+    }
+
+    for number in (1..4).rev() {
+        println!("number {}", number);
+    }
+
+    let s = "hello";
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+
+    let s = String::from("hello");
+    takes_ownership(s);
+    // println!("{s}");
+    let x = 5;
+    make_copy(x);
+
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+    let s1 = String::from("hello");
+
+    let (s2,len) = calculate_length(s1);
+
+    println!("the length of {} is {}", s2, len);
+
+
+    let s1 = String::from("Hello");
+
+    let len = calculate_length_2(&s1);
+    println!("len of {} is {}", s1, len);
 
 }
 
@@ -196,4 +260,30 @@ fn print_labeled_measurement(v: i32, u_label: char) {
 
 fn plus_one(x: i32) -> i32 {
     x + 1
+}
+
+fn takes_ownership(s: String) {
+    println!("string: {}", s);
+}
+
+fn make_copy(x: i32) {
+    println!("x: {x}");
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(s: String) -> String {
+    s
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
+
+fn calculate_length_2 (s: &String)->usize{
+    s.len()
 }
